f1a7dea24cca3929556b218644fa7215405f4ed6
kazuki0806 <152083522+kazuki0806@users.noreply.github.com>
2024-12-23 22:59:41 +0900

    kadai16アップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,44 @@
+<?php
+
+class Food {
+    private $name;
+    private $price;
+
+    public function __construct($name, $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function show_price() {
+        return $this->price;
+    }
+}
+
+class Animal {
+    private $name;
+    private $height;
+    private $weight;
+
+    public function __construct($name, $height, $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+
+    public function show_height() {
+        return $this->height;
+    }
+}
+
+
+$food = new Food("potato", 250);
+$animal = new Animal("dog", 60, 5000);
+
+
+print_r($food);
+print_r($animal);
+
+
+echo $food->show_price() .'<br>';
+echo $animal->show_height() .'<br>';
+?>
